2631f087704ba89ee835ee57de39ef3ee5804e51
Cheeray <cheeray.huang@gmail.com>
2023-07-26 16:08:15 +0800

    upt: executor

--- executor/browser.py
+++ executor/browser.py
@@ -1,9 +1,14 @@
 import asyncio
+import json
 import re
 
 from lxml import html as html_parser
 from lxml.html.clean import Cleaner
 from pyppeteer import launch
+
+from common.logger import get_logger
+
+_log = get_logger('browser')
 
 # 测试检测webdriver
 """
@@ -35,11 +40,21 @@ async def main():
 #asyncio.get_event_loop().run_until_complete(main())
 """
 
+class BrowserException(Exception):
+    def __init__(self, ex, status_code):
+        super().__init__()
+        self.ex = ex
+        self.status_code = status_code
+
+    def __str__(self):
+        return str('browser_exception: '+ str(self.ex))
+
 class Browser(object):
     _browser = None
     _is_closed = False
     #_default_page = None
     _cleaner = Cleaner()
+    _resp_collection = {}
 
     async def __init__(self):
         pass
@@ -53,6 +68,7 @@ class Browser(object):
 
         Browser._cleaner.javascript = True
         Browser._cleaner.style = True
+
 
         return obj
 
@@ -77,8 +93,17 @@ class Browser(object):
 
         return contents
 
+    async def intercept_response(self, response, url):
+        if response.url == url:
+            Browser._resp_collection[response.url] = response
+            _log.info(f'response: {response.url}, {response.status}, {response.request.url}')
+
+
     async def get_page_contents(self, url):
         page = await Browser._browser.newPage()
+
+        #TODO: capture the response
+        #page.on('response', lambda response, url: asyncio.ensure_future(self.intercept_response(response, url)))
 
         # pass the webdriver testing
         await page.evaluateOnNewDocument('() =>{Object.defineProperties(navigator, {webdriver: {get: ()=>false}})}')
@@ -86,16 +111,30 @@ class Browser(object):
         await page.setUserAgent("Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5")
         await page.setViewport(viewport={'width': 1536, 'height': 768})
 
-        #contents = ''
+        contents = ''
         raw_html = ''
-        await page.goto(url, {'waitUntil': 'networkidle0'})
+        resp = None
+        try:
+            await page.goto(url, {'waitUntil': 'networkidle0'})
 
-        raw_html = await page.content()
-        await page.close()
+            raw_html = await page.content()
+            contents = self.analyse(raw_html)
+        except Exception as e:
+            _log.error(f'access url failed: {e}')
+            status_code = -1
+            if url in Browser._resp_collection:
+                status_code = Browser._resp_collection[url].status
+                del Browser._resp_collection[url]
+            raise BrowserException(e, status_code)
 
-        contents = self.analyse(raw_html)
+        finally:
+            if url in Browser._resp_collection:
+                resp = Browser._resp_collection[url]
+                del Browser._resp_collection[url]
 
-        return contents
+            await page.close()
+
+        return contents, 200
 
 async def main():
     b = await Browser()
--- executor/client.py
+++ executor/client.py
@@ -6,13 +6,14 @@ import asyncio
 import json
 import signal
 import sys
+import traceback
 
-from kafka import KafkaConsumer
+from kafka import KafkaProducer, KafkaConsumer
 from kafka import TopicPartition
 
 from common.settings import UATConfig
 from common.logger import get_logger
-from browser import Browser
+from browser import Browser, BrowserException
 
 _log = get_logger('client')
 
@@ -33,6 +34,7 @@ signal.signal(signal.SIGINT, _quit_signal_handler)
 
 class Client:
     _client = None
+    _resp_client = None
     _tp = None
     _partition = 0
 
@@ -42,8 +44,7 @@ class Client:
         obj = super().__new__(cls)
         Client._client = KafkaConsumer(
             group_id=UATConfig.KAFKA_TASK_GROUP_ID,
-            bootstrap_servers=UATConfig.KAFKA_RESULT_HOST,
-            #auto_offset_reset='earliest',
+            bootstrap_servers=UATConfig.KAFKA_TASK_HOST,
             enable_auto_commit=False,
             value_deserializer = lambda m: json.loads(m.decode('ascii')),
         )
@@ -55,6 +56,10 @@ class Client:
 
         Client._browser = await Browser()
 
+        Client._resp_client = KafkaProducer(
+            bootstrap_servers=UATConfig.KAFKA_RESULT_HOST,
+            value_serializer=lambda m: json.dumps(m).encode('ascii'),
+        )
 
         return obj
 
@@ -64,11 +69,35 @@ class Client:
 
 
     async def handle(self, data):
-        sid = data['sid']
+        if ('sid' not in data) or ('urls' not in data):
+            _log.warning('there are NOT "sid" or "urls" in data, drop this task.')
+            return None
 
+        sid = data['sid']
+        max_words = data['max_words']
+
+        res = {
+            'sid': sid,
+        }
         for url in data['urls']:
-            contents = await Client._browser.get_page_contents(url)
-            _log.info(f'contents:\n{contents}')
+            try:
+                res[url]={}
+                contents, status_code = await Client._browser.get_page_contents(url)
+                #_log.info(f'status_code: {status_code}, contents:\n{contents}')
+                if len(contents) > max_words:
+                    contents = contents[0:max_words]
+                res[url]['ctn'] = contents
+                res[url]['status_code'] = status_code
+                res[url]['ex'] = ''
+            except BrowserException as e:
+                _log.warning(f'access url <{url}> failed: {str(e)}, status_code: {e.status_code}, skip this url.')
+                res[url]['ex'] = str(e)
+                res[url]['status_code'] = e.status_code
+                res[url]['ctn'] = ''
+                continue
+
+        return res
+
 
     async def run(self):
         while True:
@@ -80,9 +109,15 @@ class Client:
 
                     Client._client.commit()
 
-                    await self.handle(task.value['data'])
+                    res = await self.handle(task.value['data'])
+                    if res is not None:
+                        _log.info(f'task result:\n{json.dumps(res, indent=2)}')
+                        Client._resp_client.send(UATConfig.KAFKA_TASK_RESP_TOPIC, res)
+
             except Exception as e:
                 _log.warning(f'an ERROR happenend in kafka consumption: [{e}]. RESTART consumption loop.')
+
+                traceback.print_exc()
                 Client._client.commit()
 
 
